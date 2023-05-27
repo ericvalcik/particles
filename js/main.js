07f2b0976122a9cea37445fd4295fa1c51d94a6e
eric valcik <eric.valcik@gmail.com>
2023-05-28 00:40:39 +0200

    fix build 6

--- js/main.js
+++ js/main.js
@@ -10,13 +10,13 @@ let imgUrl = '/red-star-final.png';
 let img;
 let particles = [];
 
-function preload() {
-  img = loadImage(imgUrl);
+p5.preload = () => {
+  img = p5.loadImage(imgUrl);
 }
 
-function setup() {
-  const size = Math.min(windowWidth, windowHeight) * 0.8;
-  let cnv = createCanvas(size, size);
+p5.setup = () => {
+  const size = Math.min(p5.windowWidth, p5.windowHeight) * 0.8;
+  let cnv = p5.createCanvas(size, size);
   cnv.style("display", "block");
   cnv.style("position", "absolute");
   cnv.style("inset", 0);
@@ -25,8 +25,8 @@ function setup() {
   spawnParticles();
 }
 
-function draw() {
-  background(255);
+p5.draw = () => {
+  p5.background(255);
   //image(static, 0, 0, width, height);
   particles.forEach((particle) => {
     particle.update();
@@ -34,11 +34,11 @@ function draw() {
   });
 }
 
-function spawnParticles() {
-  for (let i = 0; i < width; i += RESOLUTION) {
-    for (let j = 0; j < height; j += RESOLUTION) {
-      let x = (i / width) * img.width;
-      let y = (j / height) * img.height;
+p5.spawnParticles = () => {
+  for (let i = 0; i < p5.width; i += RESOLUTION) {
+    for (let j = 0; j < p5.height; j += RESOLUTION) {
+      let x = (i / p5.width) * img.width;
+      let y = (j / p5.height) * img.height;
       const color = img.get(x, y);
       if (color[0] + color[1] + color[2] > 255 * 3 - 50) continue;
       particles.push(
@@ -59,9 +59,9 @@ class Particle {
 
   update() {
     // get vectors for mouse, target, and current position
-    let mouseVector = createVector(mouseX, mouseY);
-    let currentVector = createVector(this.x, this.y);
-    let targetVector = createVector(this.targetX, this.targetY);
+    let mouseVector = p5.createVector(p5.mouseX, p5.mouseY);
+    let currentVector = p5.createVector(this.x, this.y);
+    let targetVector = p5.createVector(this.targetX, this.targetY);
 
     // calculate vector from mouse to particle and its magnitude (distance)
     let fromMouseToParticle = p5.Vector.sub(currentVector, mouseVector);
@@ -71,19 +71,19 @@ class Particle {
     let fromParticleToTarget = p5.Vector.sub(targetVector, currentVector);
     let distanceToTarget = fromParticleToTarget.mag();
 
-    let totalForce = createVector(0, 0);
+    let totalForce = p5.createVector(0, 0);
 
     // if mouse is within 100 pixels, calculate a repulsive force
 
     if (distanceToMouse < EFFECT_DISTANCE) {
-      let respulsionForce = map(distanceToMouse, 0, EFFECT_DISTANCE, MAX_FORCE, MIN_FORCE);
-      fromMouseToParticle.setMag(respulsionForce);
+      let repulsionForce = p5.map(distanceToMouse, 0, EFFECT_DISTANCE, MAX_FORCE, MIN_FORCE);
+      fromMouseToParticle.setMag(repulsionForce);
       totalForce.add(fromMouseToParticle);
     }
 
     // if particle is not at tartget, calculate an attractive force
     if (distanceToMouse > 0) {
-      let attractionForce = map(distanceToTarget, 0, EFFECT_DISTANCE, MIN_FORCE, MAX_FORCE);
+      let attractionForce = p5.map(distanceToTarget, 0, EFFECT_DISTANCE, MIN_FORCE, MAX_FORCE);
       fromParticleToTarget.setMag(attractionForce);
       totalForce.add(fromParticleToTarget);
     }
@@ -94,15 +94,15 @@ class Particle {
   }
 
   draw() {
-    fill(this.color);
-    noStroke();
-    ellipse(this.x, this.y, PARTICLE_SIZE);
+    p5.fill(this.color);
+    p5.noStroke();
+    p5.ellipse(this.x, this.y, PARTICLE_SIZE);
   }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight)
+  p5.resizeCanvas(p5.windowWidth, p5.windowHeight)
   particles = [];
-  spawnParticles();
-  draw();
+  p5.spawnParticles();
+  p5.draw();
 }
